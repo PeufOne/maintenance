7d027af7bfea0534a6133424a251db55719e9d20
PeufOne <jonas.voisard@gmail.com>
2019-05-06 20:10:56 +0200

    front-end

--- public/javascripts/tools.js
+++ public/javascripts/tools.js
@@ -2,10 +2,12 @@ var tools = [
    {
       "name": "Tool A",
       "worksId": [1, 2, 3]
-   },{
+   },
+   {
       "name": "Tool B",
       "worksId": [1, 2, 3]
-   },{
+   },
+   {
       "name": "Tool C",
       "worksId": [1, 2, 3]
    }
--- public/javascripts/works.js
+++ public/javascripts/works.js
@@ -5,34 +5,21 @@ var works = [
       "frq": 1,
       "title": "Title A",
       "img": "image name",
-      "step": [
-         "Step 1",
-         "Step 2",
-         "Step 3"
-      ]
+      "step": ["Step 1", "Step 2", "Step 3"]
    },
-      {
+  {
       "id": 2,
       "level": 2,
       "frq": 1,
-      "title": "Title B",
+      "title": "Title A",
       "img": "image name",
-      "step": [
-         "Step 1",
-         "Step 2",
-         "Step 3"
-      ]
-   },
-      {
+      "step": ["Step 1", "Step 2", "Step 3"]
+   },   {
       "id": 3,
       "level": 2,
       "frq": 2,
-      "title": "Title B",
+      "title": "Title A",
       "img": "image name",
-      "step": [
-         "Step 1",
-         "Step 2",
-         "Step 3"
-      ]
+      "step": ["Step 1", "Step 2", "Step 3"]
    }
-]
+] 
